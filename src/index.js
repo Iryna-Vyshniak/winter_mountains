bc7e61c0d9dc0ee29dd7cfd34ee009a31b8354b9
Iryna Vyshniak <iryna.vyshniak@gmail.com>
2023-01-05 12:12:24 +0200

    add function to play audio

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const refs = {
   mountainRight: document.querySelector('#mountain_right'),
   clouds1: document.querySelector('#clouds_1'),
   clouds2: document.querySelector('#clouds_2'),
+  clouds3: document.querySelector('#clouds_3'),
+  clouds4: document.querySelector('#clouds_4'),
 };
 
 window.addEventListener('scroll', parallax);
@@ -17,4 +19,25 @@ function parallax(e) {
   refs.clouds1.style.left = `${value * 2}px`;
   refs.text.style.bottom = `-${value}px`;
   refs.man.style.height = `${window.innerHeight - value}px`;
+  refs.clouds3.style.right = `${value * 2}px`;
+  refs.clouds4.style.right = `${value * 2}px`;
 }
+
+// AUDIO
+const soundButton = document.querySelector('.sound');
+const audio = document.querySelector('.audio');
+
+soundButton.addEventListener('click', e => {
+  soundButton.classList.toggle('paused');
+  audio.paused ? audio.play() : audio.pause();
+});
+
+// when focusing on this an open page in the browser
+window.onfocus = function () {
+  soundButton.classList.contains('paused') ? audio.pause() : audio.play();
+};
+
+//when we open a new tab in the browser - the audio is turned off
+window.onblur = function () {
+  audio.pause();
+};
